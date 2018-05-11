1b4bbb53226cd756fe12b955c589ed2731fa0380
David Van Isacker <davidvanisacker@gmail.com>
2018-05-12 01:57:36 +0900

    [#157515330] Recreated chain with all cryptodollar and dex contracts deployed

--- scripts/allocate-ether.js
+++ scripts/allocate-ether.js
@@ -3,4 +3,7 @@ function allocateEther() {
   eth.sendTransaction({ from: accounts[0], to: accounts[1], value: 10e18 })
   eth.sendTransaction({ from: accounts[0], to: accounts[2], value: 10e18 })
   eth.sendTransaction({ from: accounts[0], to: accounts[3], value: 10e18 })
+  eth.sendTransaction({ from: accounts[0], to: accounts[4], value: 10e18 })
+  eth.sendTransaction({ from: accounts[0], to: accounts[5], value: 10e18 })
+  eth.sendTransaction({ from: accounts[0], to: accounts[6], value: 10e18 })
 }
